6deb843ec149673adcc36a29445e43a9e9af9fbe
Neemie <tasneemmohamed910@gmail.com>
2023-04-25 11:43:17 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,13 @@
  * followed by a new line
  * @n; print from this number
 */
-void print_to_98(int n)
-{
-	int i,j;
 
-	if(n <= 98)
+void print_to_98(int n)
+
+{
+	int i, j;
+
+	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
